f9a0f0a7f81877d9a99c3be93b8e82e78e330520
ChihWei <cwl.chihweil@gmail.com>
2018-04-21 18:04:57 -0600

    small fix

--- ezcook17/urls.py
+++ ezcook17/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 
 
-    url(r'^main/$', views.post_list_without_edit, name='post_list_without_edit'),
+    url(r'^home/$', views.post_list_without_edit, name='post_list_without_edit'),
     url(r'^post_without/(?P<pk>\d+)/$', views.post_detail_without_edit, name='post_detail_without_edit'),
     url(r'^login/$', views.login_form, name='login_form'),
     url(r'^signup/$', views.signup, name='signup'),

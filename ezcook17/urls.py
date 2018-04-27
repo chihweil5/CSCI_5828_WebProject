6a5e764eea98394878365e29b6008d7d5d3cfa10
ChihWei <cwl.chihweil@gmail.com>
2018-04-27 00:27:11 -0600

    Change url

--- ezcook17/urls.py
+++ ezcook17/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>(\w+-){4}\w+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^home/$', views.post_list_without_edit, name='post_list_without_edit'),
-    url(r'^post_without/(?P<pk>(\w+-){4}\w+)/$', views.post_detail_without_edit, name='post_detail_without_edit'),
+    url(r'^home/(?P<pk>(\w+-){4}\w+)/$', views.post_detail_without_edit, name='post_detail_without_edit'),
 
     url(r'^login/$', views.login_form, name='login_form'),
     url(r'^signup/$', views.signup, name='signup'),
